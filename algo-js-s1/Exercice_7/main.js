0c37e06ba1c630876bc3356e941e65944acaedf5
NathanBoisleux <nathan.boisleux@edu.devinci.fr>
2023-09-27 11:29:37 +0200

    Adding Exercise 7

---
+++ algo-js-s1/Exercice_7/main.js
@@ -0,0 +1,11 @@
+let onePieceCharacter = {
+    firstName : "Luffy",
+    lastName : "Monkey D.",
+    age : 19 ,
+    place : "un bateau",
+    status : "ROI DES PIRATES"
+}
+
+var sentence = onePieceCharacter.lastName + " " + onePieceCharacter.firstName + " a " + onePieceCharacter.age + " ans et est sur " + onePieceCharacter.place + " et est le " + onePieceCharacter.status + " !!!"
+
+console.log(sentence)
