13488961a1ae0bf4508fec85c560d7f2a0d3e34c
NathanBoisleux <nathan.boisleux@edu.devinci.fr>
2023-09-27 10:53:43 +0200

    Adding Exercise 6

---
+++ algo-js-s1/Exercice_6/main.js
@@ -0,0 +1,9 @@
+let names = []
+
+names.push("Vincent", "Paul", "Arthur")
+//console.log(names)
+
+for(let i = 0; i < names.length; i++) {
+    names[i] += " va à la pêche"
+    console.log(names[i])
+}
